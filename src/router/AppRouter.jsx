b88451090abc67eccc66cd4631b2413d5b20375d
Burakkturk <burak1845turk@icloud.com>
2024-05-10 20:50:12 +0300

    feat: navigate for login added

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -7,10 +7,12 @@ import PersonDetail from "../pages/PersonDetail";
 import Contact from "../pages/Contact";
 import NotFound from "../pages/NotFound";
 import Footer from "../components/Footer";
-import Fullstack from "../pages/FullStack";
+import FullStack from "../pages/FullStack";
 import Aws from "../pages/Aws";
 import Next from "../pages/Next";
 import React from "../pages/React";
+import PrivateRouter from "./PrivateRouter";
+import Login from "../pages/Login";
 import { useState } from "react";
 
 const AppRouter = () => {
@@ -23,18 +25,19 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/paths" element={<Paths />}>
-          <Route index element={<Fullstack />} />
-          <Route path="fullstack" element={<Fullstack />}>
+          <Route index element={<FullStack />} />
+          <Route path="fullstack" element={<FullStack />}>
             <Route index element={<React />} />
             <Route path="next" element={<Next />} />
           </Route>
           <Route path="aws" element={<Aws />} />
         </Route>
-       
+        <Route element={<PrivateRouter user={user} />}>
           <Route path="/people" element={<People />} />
           <Route path="/people/:id" element={<PersonDetail />} />
-       
+        </Route>
         <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login setUser={setUser} />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
